d7c1cd8fbddd711d743b32e336553c291c225731
Elisa Tognini <100294399+ElisaTognini@users.noreply.github.com>
2023-05-13 17:00:49 +0200

    added main error page components
    needs styling and link to last valid screen

---
+++ src/error.vue
@@ -0,0 +1,22 @@
+<script setup>
+    defineProps(['error'])
+</script>
+
+<template>
+    <Navbar />
+    <main>
+        <h1 id="err-code">{{ error.statusCode }}</h1>
+        <h2 v-if="error.statusCode === 404">Page not found</h2>
+        <h2 v-else>An error occurred</h2>
+    </main>
+    <div class="err-links">
+        <NuxtLink to="/">Back to homepage</NuxtLink>
+        <NuxtLink to="/">Back to last valid screen</NuxtLink>
+    </div> 
+</template>
+
+<style scoped>
+main{
+    text-align: center;
+}
+</style>
