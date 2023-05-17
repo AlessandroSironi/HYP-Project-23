141525367bebd0e2e794ce030304eb0deb491c9b
Scherini Samuele <samuele.scherini@mail.polimi.it>
2023-05-17 14:43:08 +0200

    deleted useless rule

--- src/types/schema.ts
+++ src/types/schema.ts
@@ -1,160 +1,153 @@
-export type Json =
-  | string
-  | number
-  | boolean
-  | null
-  | { [key: string]: Json }
-  | Json[]
+export type Json = string | number | boolean | null | { [key: string]: Json } | Json[];
 
 export interface Database {
-  public: {
-    Tables: {
-      area: {
-        Row: {
-          created_at: string | null
-          description: string
-          id: number
-          name: string
-          pic: string
-        }
-        Insert: {
-          created_at?: string | null
-          description: string
-          id?: number
-          name: string
-          pic: string
-        }
-        Update: {
-          created_at?: string | null
-          description?: string
-          id?: number
-          name?: string
-          pic?: string
-        }
-      }
-      employee: {
-        Row: {
-          created_at: string | null
-          description: string
-          id: number
-          mail: string
-          name: string
-          pic: string
-          role: string
-          surname: string
-        }
-        Insert: {
-          created_at?: string | null
-          description: string
-          id?: number
-          mail: string
-          name: string
-          pic: string
-          role: string
-          surname: string
-        }
-        Update: {
-          created_at?: string | null
-          description?: string
-          id?: number
-          mail?: string
-          name?: string
-          pic?: string
-          role?: string
-          surname?: string
-        }
-      }
-      project: {
-        Row: {
-          companyImage: string
-          companyLogo: string
-          companyName: string
-          created_at: string | null
-          description: string
-          founderName: string
-          founderSurname: string
-          id: number
-          mostRelevant: boolean | null
-          name: string
-          review: string
-          year: number
-        }
-        Insert: {
-          companyImage: string
-          companyLogo: string
-          companyName: string
-          created_at?: string | null
-          description: string
-          founderName: string
-          founderSurname: string
-          id?: number
-          mostRelevant?: boolean | null
-          name: string
-          review: string
-          year: number
-        }
-        Update: {
-          companyImage?: string
-          companyLogo?: string
-          companyName?: string
-          created_at?: string | null
-          description?: string
-          founderName?: string
-          founderSurname?: string
-          id?: number
-          mostRelevant?: boolean | null
-          name?: string
-          review?: string
-          year?: number
-        }
-      }
-      related_to: {
-        Row: {
-          area: number
-          created_at: string | null
-          project: number
-        }
-        Insert: {
-          area: number
-          created_at?: string | null
-          project: number
-        }
-        Update: {
-          area?: number
-          created_at?: string | null
-          project?: number
-        }
-      }
-      works_in: {
-        Row: {
-          created_at: string | null
-          emp: number
-          project: number
-        }
-        Insert: {
-          created_at?: string | null
-          emp: number
-          project: number
-        }
-        Update: {
-          created_at?: string | null
-          emp?: number
-          project?: number
-        }
-      }
-    }
-    Views: {
-      [_ in never]: never
-    }
-    Functions: {
-      [_ in never]: never
-    }
-    Enums: {
-      [_ in never]: never
-    }
-    CompositeTypes: {
-      [_ in never]: never
-    }
-  }
+    public: {
+        Tables: {
+            area: {
+                Row: {
+                    created_at: string | null;
+                    description: string;
+                    id: number;
+                    name: string;
+                    pic: string;
+                };
+                Insert: {
+                    created_at?: string | null;
+                    description: string;
+                    id?: number;
+                    name: string;
+                    pic: string;
+                };
+                Update: {
+                    created_at?: string | null;
+                    description?: string;
+                    id?: number;
+                    name?: string;
+                    pic?: string;
+                };
+            };
+            employee: {
+                Row: {
+                    created_at: string | null;
+                    description: string;
+                    id: number;
+                    mail: string;
+                    name: string;
+                    pic: string;
+                    role: string;
+                    surname: string;
+                };
+                Insert: {
+                    created_at?: string | null;
+                    description: string;
+                    id?: number;
+                    mail: string;
+                    name: string;
+                    pic: string;
+                    role: string;
+                    surname: string;
+                };
+                Update: {
+                    created_at?: string | null;
+                    description?: string;
+                    id?: number;
+                    mail?: string;
+                    name?: string;
+                    pic?: string;
+                    role?: string;
+                    surname?: string;
+                };
+            };
+            project: {
+                Row: {
+                    companyImage: string;
+                    companyLogo: string;
+                    companyName: string;
+                    created_at: string | null;
+                    description: string;
+                    founderName: string;
+                    founderSurname: string;
+                    id: number;
+                    mostRelevant: boolean | null;
+                    name: string;
+                    review: string;
+                    year: number;
+                };
+                Insert: {
+                    companyImage: string;
+                    companyLogo: string;
+                    companyName: string;
+                    created_at?: string | null;
+                    description: string;
+                    founderName: string;
+                    founderSurname: string;
+                    id?: number;
+                    mostRelevant?: boolean | null;
+                    name: string;
+                    review: string;
+                    year: number;
+                };
+                Update: {
+                    companyImage?: string;
+                    companyLogo?: string;
+                    companyName?: string;
+                    created_at?: string | null;
+                    description?: string;
+                    founderName?: string;
+                    founderSurname?: string;
+                    id?: number;
+                    mostRelevant?: boolean | null;
+                    name?: string;
+                    review?: string;
+                    year?: number;
+                };
+            };
+            related_to: {
+                Row: {
+                    area: number;
+                    created_at: string | null;
+                    project: number;
+                };
+                Insert: {
+                    area: number;
+                    created_at?: string | null;
+                    project: number;
+                };
+                Update: {
+                    area?: number;
+                    created_at?: string | null;
+                    project?: number;
+                };
+            };
+            works_in: {
+                Row: {
+                    created_at: string | null;
+                    emp: number;
+                    project: number;
+                };
+                Insert: {
+                    created_at?: string | null;
+                    emp: number;
+                    project: number;
+                };
+                Update: {
+                    created_at?: string | null;
+                    emp?: number;
+                    project?: number;
+                };
+            };
+        };
+        Views: {
+            [_ in never]: never;
+        };
+        Functions: {
+            [_ in never]: never;
+        };
+        Enums: {
+            [_ in never]: never;
+        };
+        CompositeTypes: {
+            [_ in never]: never;
+        };
+    };
 }
-
