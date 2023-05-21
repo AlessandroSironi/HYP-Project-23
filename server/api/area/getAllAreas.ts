f5ff32771765a708fcbbb45527d7b04bdeab386e
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-05-21 14:00:19 +0200

    Fix tentativo

--- server/api/area/getAllAreas.ts
+++ server/api/area/getAllAreas.ts
@@ -1,7 +1,7 @@
 import { serverSupabaseClient } from '#supabase/server';
 import { Database } from '~/types/schema';
 
-export default eventHandler(async (event: any) => {
+export default eventHandler(async (event) => {
     // create a client for the supabase DB and infer the DB structure
     const client = serverSupabaseClient<Database>(event);
     // query the Db and then send to the Vue client an object with employees and error field
