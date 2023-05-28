2663ae9219360ac5ac3fea19435da8d7cb385210
Alessandro Sironi <100214750+AlessandroSironi@users.noreply.github.com>
2023-05-28 16:43:36 +0200

    About mid commit

--- pages/about.vue
+++ pages/about.vue
@@ -1,7 +1,48 @@
 <script setup lang="ts"></script>
 
 <template>
-    <div></div>
+    <div class="content">
+        <h2 class="orientational-info">About us</h2>
+        <h1 class="page-h1-title">European roots, global from the start</h1>
+        <p>We use our hard-won experience to help visonary founders become global category leaders.</p>
+        <div class="hero-image-div">
+            <nuxt-img class="hero-image" src="https://nwgklsfwmhatudgymnvk.supabase.co/storage/v1/object/sign/resources/about/aboutus.webp?token=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1cmwiOiJyZXNvdXJjZXMvYWJvdXQvYWJvdXR1cy53ZWJwIiwiaWF0IjoxNjg1MTA1NTc2LCJleHAiOjEwMDE2ODUxMDU1NzV9.Q5o0t9EqUXtzlIhLWPqhPnmwkuOU1rEOcwwlx1bZM1A&t=2023-05-26T12%3A52%3A56.667Z"/>
+        </div>
+        <div class="paragraph-content">
+            <h3 class="">WHAT IS OUT APPROACH</h3>
+            <p class="p-alt">We’re not a traditional venture capital firm. We are built by founders for founders. Every single aspect of Core, every part of our culture, and every decision we take, is designed with the ambition of helping founders succeed. We exist for more than returns. </p>
+            <p class="p-alt"> We believe entrepreneurs are the ultimate agents of positive, transformational change across every aspect of our society and economy. Our mission is to spur this progress forward. </p>
+            <p class="p-alt">It’s why we partner with the world’s most ambitious founders. The rule-breakers who want to build the next leader in their category. The world-shapers creating companies that fundamentally shift the way we live today. The gamechangers using technology to rewire the world in favour of something better, for as many people as possible. When we find these people, we invest much more than money. We work hand- in-glove with them, drawing on hard-won experience scaling some of the most successful technology companies in the world.</p>
+        </div>
+        
+        
+        <div class="colored-bar">
+            
+        </div>
+    </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.hero-image {
+    
+    height: auto;
+    border-radius: 1.25rem 1.25rem 1.25rem 1.25rem;
+}
+
+.hero-image-div {
+    display: flex;
+    justify-content: center;
+    margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.p-alt {
+    color: var(--par-color);
+    line-height: 1.75rem;
+}
+
+.paragraph-content {
+    max-width: min(4600px, 90%);
+}
+
+</style>
