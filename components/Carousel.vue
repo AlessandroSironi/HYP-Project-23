13dd28ec822cfccfbada094171d31eb45818d470
Elisa Tognini <100294399+ElisaTognini@users.noreply.github.com>
2023-05-31 08:03:05 +0200

    started working on top projects page

---
+++ components/Carousel.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+/**
+ * Component name: Carousel
+ * Description: this component will display
+ * the most relevant projects in a carousel-like
+ * fashion in the "Top Projects" page.
+ */
+</script>
+
+<template>
+
+</template>
+
+<style scoped>
+</style>
