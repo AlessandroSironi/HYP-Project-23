0a0daf6d8073429ff4369ace9e1875a9b9406c35
Elisa Tognini <100294399+ElisaTognini@users.noreply.github.com>
2023-06-01 22:44:39 +0200

    hopefully finished

--- components/Carousel.vue
+++ components/Carousel.vue
@@ -1,58 +1,90 @@
 <script setup lang="ts">
-/**
- * Component name: Carousel
- * Description: this component will display
- * the most relevant projects in a carousel-like
- * fashion in the "Top Projects" page.
- */   
+import { ref } from 'vue';
 
-    interface Props {
-        projects?: Project[];
-    }
+interface Props {
+  projects?: Project[];
+}
 
-    const {projects} = defineProps<Props>();
+const { projects } = defineProps<Props>();
 
-    // Store the current translateX value of the slider
-    const translateX = ref(-195);
+const translateX = ref(-200);
 
-    // Move the slider based on the hovered project
-    const moveSlider = (event: MouseEvent, index: number) => {
-        const containerWidth = (event.currentTarget as HTMLElement).clientWidth;
-        const projectWidth = containerWidth / (projects?.length || 1);
-        const hoverMultiplier = 2.2;
+const moveSlider = (direction: 'prev' | 'next') => {
+  const containerWidth = (sliderContainerRef.value as HTMLElement).clientWidth;
+  const projectWidth = 267;
+  const gap = 70;
+  const visibleProjects = Math.floor(containerWidth / (projectWidth + gap));
+  const totalProjects = (projects?.length || 1);
+  const maxTranslateX = (totalProjects - visibleProjects) * projectWidth;
+  
+  let targetX = translateX.value;
+  if (direction === 'prev') {
+    targetX += (projectWidth);
+    targetX = Math.min(gap, targetX);
+  } else if (direction === 'next') {
+    targetX -= (projectWidth);
+    targetX = Math.max(-maxTranslateX, targetX);
+  }
+  
+  translateX.value = targetX;
+};
 
-        // Adjust the projectWidth and targetX values
-        const adjustedProjectWidth = projectWidth * hoverMultiplier;
-        const targetX = -(index * adjustedProjectWidth) + containerWidth / 2 - adjustedProjectWidth / 2;
-
-        translateX.value = targetX;
-    };
+const sliderContainerRef = ref<HTMLElement | null>(null);
 </script>
 
 <template>
-    <main class="slider-container" @mouseleave="translateX = -195">
-        <div class="slider" :style="{ transform: `translateX(${translateX}px)` }">
-          <div
-            v-for="(project, index) in projects ?? []"
-            :key="project?.id"
-            class="slider-item"
-            @mouseover="moveSlider($event, index)"
-          >
-            <ProjectCard :project="project" />
-          </div>
-        </div>
-      </main>
+  <main class="slider-container" ref="sliderContainerRef">
+    <div class="slider" :style="{ transform: `translateX(${translateX}px)` }">
+      <div
+        v-for="(project, index) in projects ?? []"
+        :key="project?.id"
+        class="slider-item"
+      >
+        <ProjectCard :project="project" />
+      </div>
+    </div>
+    <div class="slider-controls">
+        <Icon class="command-arrow" name="bxs:left-arrow" @click="moveSlider('prev')"/>
+        <Icon class="command-arrow" name="bxs:right-arrow" @click="moveSlider('next')"/>
+    </div>
+  </main>
 </template>
 
 <style scoped>
 .slider-container {
-    width: var(--content-width);
-    overflow-x: hidden;
-    padding: 1.5rem;
+  max-width: 90%;
+  padding: 1.5rem;
+  overflow: hidden;
+}
+
+.slider {
+  display: flex;
+  transition: transform 0.7s ease-in-out;
+  gap: 1.5rem;
+}
+
+.slider-item {
+  flex: 0 0 auto;
+}
+
+.slider-controls {
+  margin-top: 1rem;
+  text-align: center;
+}
+
+.slider-controls button {
+  margin: 0 0.5rem;
+}
+
+@media screen and (max-width: 768px) {
+  .slider-container {
+    padding: 1rem;
   }
-  .slider {
-    display: flex;
-    gap: 1.25rem;
-    transition: transform 0.5s ease-in-out;
-  }
+}
+
+.command-arrow{
+    color: var(--btn-color-alt);
+    margin: 1rem;
+    cursor: pointer;
+}
 </style>
